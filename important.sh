a2f09b53815499fca62b3ebb2d5f21a84a4888d7
Haitham Ramadan <34133873+maaxx0@users.noreply.github.com>
2024-04-06 19:56:58 +0200

    Add files via upload

---
+++ important.sh
@@ -0,0 +1,13 @@
+sudo apt install build-essential
+
+sudo apt install libedit-dev
+
+sudo apt install uuid*
+
+sudo apt install libjansson*
+
+sudo apt install libxml2*
+
+
+sudo apt install libsqlite3-dev
+
